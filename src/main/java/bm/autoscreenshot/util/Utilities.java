77d79c69e2f079383d8954b6ba463fe803916c16
Brandon Moore <brmo00206@gmail.com>
2016-02-07 10:35:36 -0400

    Added bm.autoscreenshot.util as a tracked package.

---
+++ src/main/java/bm/autoscreenshot/util/Utilities.java
@@ -0,0 +1,12 @@
+package bm.autoscreenshot.util;
+
+public class Utilities 
+{
+	
+//TODO: Create utilities for incremental file naming system.
+	
+	public static int findBaseFileNum(String dir)
+	{
+		return 0;
+	}
+}
