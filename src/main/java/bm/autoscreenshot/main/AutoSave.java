1b4a036dc60f853545c2474223c3ba8e81a91c6a
Brandon Moore <brmo00206@gmail.com>
2016-02-07 10:30:00 -0400

    Cleaned code, renamed packages and created utilities class.

--- src/main/java/bm/autoscreenshot/main/AutoSave.java
+++ src/main/java/bm/autoscreenshot/main/AutoSave.java
@@ -1,4 +1,4 @@
-package brandonmoore.autosavescreenshot.main;
+package bm.autoscreenshot.main;
 
 import java.awt.AWTException;
 import java.awt.Rectangle;
@@ -11,28 +11,39 @@ import java.util.Random;
 
 import javax.imageio.ImageIO;
 
-public class AutoSave {
+public class AutoSave 
+{
 	
-	 public static void main(String[] args) {
-	        try {
+	 public static void main(String[] args) 
+	 {
+	        try 
+	        {	
 	        	Random ran = new Random();
 	        	int num = ran.nextInt();
 	            Robot robot = new Robot();
+	            
 	            String format = "jpg";
 	            String fileName = "FullScreenshot" + num + "." + format;
 	             
 	            Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
 	            BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
 	            ImageIO.write(screenFullImage, format, new File(fileName));
+	            
+	            //Triggers windows explorer folder that file was saved in
 	            Process p = new ProcessBuilder("explorer.exe", "/select,C:\\screenshots\\"+fileName).start();
 	             
-
 	        } 
-	        catch(AWTException e){
-	        	 System.err.println(e.getMessage());
+	        catch(AWTException e1)
+	        {
+	        	 System.err.println(e1.getMessage());
 	        }
-	        catch (IOException ex) {
-	            System.err.println(ex.getMessage());
+	        catch (IOException e2) 
+	        {
+	            System.err.println(e2.getMessage());
+	        }
+	        catch (Exception e3)
+	        {
+	        	System.err.println(e3.getMessage());
 	        }
 	    }
 
